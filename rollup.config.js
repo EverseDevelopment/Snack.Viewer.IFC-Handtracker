aabfb71acf28ba895ce059dd102f2081a4a2b6bd
Pablo Derendinger <pderendinger@e-verse.com>
2022-08-22 16:47:17 -0300

    Fixed copy during build

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,25 @@
 // noinspection JSUnusedGlobalSymbols
-
+import copy from 'rollup-plugin-copy'
 import resolve from '@rollup/plugin-node-resolve'; // locate and bundle dependencies in node_modules (mandatory)
 
 export default {
   input: 'src/main.js',
+  plugins: [
+    resolve(),
+    copy({
+      targets: [
+        { src: 'node_modules/web-ifc/web-ifc-mt.wasm', dest: './' },
+        { src: 'node_modules/web-ifc/web-ifc.wasm', dest: './' },
+        { src: 'node_modules/web-ifc-three/IFCWorker.js', dest: './' },
+        { src: 'node_modules/web-ifc-three/IFCWorker.js.map', dest: './' },
+      ]
+    })
+
+  ],
   output: [
     {
       format: 'cjs',
       file: 'bundle.js'
     },
   ],
-  plugins: [
-    resolve()
-
-  ]
 };
